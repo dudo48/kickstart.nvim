2ebe7df6e45ef250528ef6ba92ecd8ec19302a5f
Fady Emad <fadyemad14705@gmail.com>
2024-09-28 18:41:16 +0300

    (nvim-surround) add

--- vscode/init.lua
+++
@@ -1,8 +0,0 @@
--- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
